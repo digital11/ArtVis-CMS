61b0fbb29db540f31f76e46bcfb824287b4a4334
digital11 <digital11@gmail.com>
2013-02-08 20:25:19 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artvis::Application.config.secret_token = 'e78818b17dedeba1029184efbec2ab740a5c6c31b7fbcc81f4e2f70146fc5e95900cc95bb336ee2d324e1b21cd04024db2299fb570030cc7cad28129a1fb1605'
